d88a883b2a69aee3699bdfa7dcffbbddb6024b4d
Benjamin Deschereux <bdeschereux@gmail.com>
2020-06-02 16:27:08 +0200

    Class shop

--- PHP/fonctions.php
+++ PHP/fonctions.php
@@ -1,43 +1,83 @@
+
+<!-- Code JS pour afficher ou non les boutons d'ajout d'articles acheté -->
+<script type="text/javascript" src="js/AcheterBtn.js"></script>
+
 <?php
-	function cardProduit()
+	class shop
 	{
-		// Récupère les informations du fichier produits.ini
-		$prod = parse_ini_file("produits.ini", true);
-		foreach ($prod as $row => $innerArray)
+		function __construct() 
 		{
-			$c = sizeof($innerArray);
-			switch ($row) {
-				case "nom_produits":
-					$nomProd = $innerArray;
-					break;
-				case "img_path":
-					$imgPath = $innerArray;
-					break;
-				case "prix":
-					$prix = $innerArray;
-					break;
+			$all = file_get_contents("produits.json", true);
+			$all = json_decode($all, true);
+			foreach ($all as $row => $innerArray)
+			{
+				$this->{$row} = $innerArray; 
 			}
+			$countProds = sizeof($this->Produits);
+			$countInfos = sizeof($this->Infos);
 		}
-		for ($i = 1; $i <= $c; $i++)
+		
+		function getProduitsCard()
 		{
-			$prod = 'prod'.$i;
-			$img = 'img'.$i;
-			$pri = 'prix'.$i;
-			echo("<div class='card border'>
-					<div class='card-body'>
-						<h5 class='card-title text-center'>$nomProd[$prod]</h5>
-						<img class='img-thumbnail align-middle' src='$imgPath[$img]'>
-						</br>			
-					</div>
-					<div class='card-footer'>
-						<div class='mt-2 float-left'>
-							Prix: $prix[$pri]
-						</div>
-						<div class='float-right'>
-							<input onclick='' type='button' value='Acheter' class='btn btn-primary text-right' id='myButton$img' style='display: block;'></input>
-						</div>
-					</div>
-				</div>");
+			$countProds = sizeof($this->Produits);
+			$i = 0;
+			$retour = "";
+			foreach($this->Produits as $row => $produit)
+			{
+				$i++;
+				$img = 'img'.$i;
+				
+				$retour.=("<div class='card border'>
+							<div class='card-body'>
+								<h5 class='card-title text-center'>$produit[nom]</h5>
+								<div class='d-flex'>
+									<img class='img-thumbnail align-middle' src='$produit[img_path]'>
+									<div class='d-flex justify-content-center'>
+										<p class='description pt-4 ml-2'>$produit[description]</p>
+									</div>
+								</div>
+							</div>
+							<div class='card-footer'>
+								<div class='mt-2 float-left'>
+									Prix: $produit[prix]
+								</div>
+								<div class='float-right'>
+									<input onclick='AcheterArticle(Acheter$img, $i)' type='button' value='Acheter' class='btn btn-primary text-right' id='Acheter$img' style='display: block;'></input>
+									<div class='countAchat$i' id='' style='display: block;'>
+										<div class='d-flex flex-row'>
+											<input onclick='MoinsArticle(Acheter$img, $i)' type='button' value='-' class='btn btn-primary text-right' id='btn-' style='display: block;'></input>
+											<p class='h6 px-2 mt-2' id='counter$i'></p>
+											<input onclick='PlusArticle($i)' type='button' value='+' class='btn btn-primary text-right' id='btn+' style='display: block;'></input>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>");
+				
+			}
+			$retour.='<script type="text/javascript">CacherNb('."$countProds".');</script>';
+			return($retour);
+		}
+		
+		function getNomEntreprise()
+		{
+			return($this->Infos['Nom_Entreprise']);
+		}
+		
+		function getInfo()
+		{
+			return($this->Infos);
+		}
+		
+		function getProduitsNom()
+		{
+			$retour = "";
+			
+			foreach($this->Produits as $row => $produit)
+			{
+				$retour .= "<a class='dropdown-item' href='#'>$produit[nom]</a>";
+			}
+			return($retour);
 		}
 	}
 ?>
