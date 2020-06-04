6be69c04289ee96da87f3da48dabe9797eebbff3
Benjamin Deschereux <bdeschereux@gmail.com>
2020-06-04 16:43:04 +0200

    Fusion

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
 	<script type="text/javascript" src="js/jquery-3.5.1.js"></script>
 	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 	
+	<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+	<script type="text/javascript" src="DataTables/datatables.min.js"></script>
+	
 	<!-- Code JS pour afficher ou non les boutons d'ajout d'articles acheté -->
 	<script type="text/javascript" src="js/AcheterBtn.js"></script>
 </head>
@@ -20,10 +23,13 @@
 	<!-- Navbar -->
 	<div class="navbar navbar-expand-md navbar-dark bg-dark">
 		
-		<!-- Bouton de commande -->
-		<a href="Commande.php" class="btn btn-secondary text-light lx-auto h-75" type="button">
-			<strong>Commander</strong>
-		</a>
+		<div class="achat">
+			<input onclick='Commande(); AfficheTableau();' type='button' class="btn btn-secondary text-light lx-auto h-75" value='Commander'></input>
+		</div>
+		
+		<div class="commande">
+			<input onclick='Retour()' type='button' class="btn btn-secondary text-light lx-auto h-75" value='Retour'></input>
+		</div>
 		
 		<div class="dropdown mx-auto">
 		
@@ -36,7 +42,7 @@
 			</div>
 			
 			<!-- Bouton de la liste -->
-			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<button class="btn btn-secondary dropdown-toggle achat" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Liste des articles
 			</button>
 			
@@ -53,58 +59,62 @@
 	</div>
 	
 	<!-- Tableau d'articles -->
-	<div class='card-group d-flex flex-wrap justify-content-center mb-5'>
-	<?php
-		// Appel de la fonction qui retourne les articles.
-		$produits = $Shop->getProduits();
-				
-		//Affichage des articles dans les card
-		$countProds = sizeof($produits);
-		$i = 0;
-		foreach($produits as $row => $produit)
-		{
-			$i++;
-			$img = 'img'.$i;
-			$newString[$i] = str_replace(" ","",$produit['nom']);
-			echo("<div class='card border '>
-					<div class='card-body' data-toggle='modal' data-target='#exampleModal$newString[$i]'>
-						<h5 class='card-title text-center'>$produit[nom]</h5>
-						<div class='d-flex'>
-							<img class='img-thumbnail align-middle' src='$produit[img_path]'>
-							<div class='d-flex justify-content-center'>
-								<p class='description pt-4 ml-2'>$produit[description]</p>
-							</div>
-						</div>
-					</div>
-					<div class='card-footer'>
-						<div class='mt-2 float-left'>
-							Prix: $produit[prix]
-						</div>
-						<div class='float-right'>
-							<input onclick='AcheterArticle(Acheter$img, $i)' type='button' value='Acheter' class='btn btn-primary text-right' id='Acheter$img' style='display: block;'></input>
-							<div class='countAchat$i' id='' style='display: block;'>
-								<div class='d-flex flex-row'>
-									<input onclick='MoinsArticle(Acheter$img, $i)' type='button' value='-' class='btn btn-primary text-right' id='btn-' style='display: block;'></input>
-									<p class='h6 px-2 mt-2' id='counter$i'></p>
-									<input onclick='PlusArticle($i)' type='button' value='+' class='btn btn-primary text-right' id='btn+' style='display: block;'></input>
+	<div class="achat">
+		<div class='card-group d-flex flex-wrap justify-content-center mb-5'>
+		<?php
+			// Appel de la fonction qui retourne les articles.
+			$produits = $Shop->getProduits();
+					
+			//Affichage des articles dans les card
+			$countProds = sizeof($produits);
+			$i = 0;
+			$nomArticle;
+			foreach($produits as $row => $produit)
+			{
+				$i++;
+				$img = 'img'.$i;
+				$nomArticle[$i] = str_replace(" ","___",$produit['nom']);
+				$prixSend[$i] = str_replace("€","",$produit['prix']);
+				echo("<div class='card border '>
+						<div class='card-body' data-toggle='modal' data-target='#exampleModal$nomArticle[$i]'>
+							<h5 class='card-title text-center'>$produit[nom]</h5>
+							<div class='d-flex'>
+								<img class='img-thumbnail align-middle' src='$produit[img_path]'>
+								<div class='d-flex justify-content-center'>
+									<p class='description pt-4 ml-2'>$produit[description]</p>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>");
-							
-		}
-		echo'<script type="text/javascript">CacherNb('."$countProds".');</script>';
-	?>
+						<div class='card-footer'>
+							<div class='mt-2 float-left'>
+								Prix: $produit[prix]
+							</div>
+							<div class='float-right'>
+								<input onclick='AcheterArticle(Acheter$img, $i, $nomArticle[$i], $prixSend[$i])' type='button' value='Acheter' class='btn btn-primary text-right' id='Acheter$img' style='display: block;'></input>
+								<div class='countAchat$i' style='display: block;'>
+									<div class='d-flex flex-row'>
+										<input onclick='MoinsArticle(Acheter$img, $i, $nomArticle[$i], $prixSend[$i])' type='button' value='-' class='btn btn-primary text-right' id='btn-' style='display: block;'></input>
+										<p class='h6 px-2 mt-2' id='$nomArticle[$i]'></p>
+										<input onclick='PlusArticle($i, $nomArticle[$i], $prixSend[$i])' type='button' value='+' class='btn btn-primary text-right' id='btn+' style='display: block;'></input>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>");
+								
+			}
+			echo'<script type="text/javascript">CacherNb('."$countProds".');</script>';
+		?>
+		</div>
 	</div>
+		
 	<?php
 		$produits = $Shop->getProduits();
 		$i = 0;
 		foreach($produits as $row => $produit)
 		{
-			$i++;
-			$newString[$i] = str_replace(" ","",$produit['nom']);
-			echo("<div class='modal fade' id='exampleModal$newString[$i]' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+			$nomArticle[$i] = str_replace(" ","___",$produit['nom']);
+			echo("<div class='modal fade' id='exampleModal$nomArticle[$i]' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
 					<div class='modal-dialog' role='document'>
 						<div class='modal-content'>
 							<div class='modal-header'>
@@ -119,8 +129,68 @@
 						</div>
 					</div>
 				</div>");
+				$i++;
 		}
 	?>
+	
+	<div class="commande">
+		<div class="jumbotron mx-4 my-4 d-block">
+			<div class="row">
+			
+				<div class="col-xl-4">
+					<table id="Commande" class="table table-bordered table-dark" width="100%"></table>
+				</div>
+				
+				<div class="col-xl-4 text-center">
+					Choix de date et heure de livraison
+					<div class="dropdown">
+					
+						<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Choisir le jour
+						</button>
+						
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<?php
+							for ($i = 0; $i <= 4; $i++)
+							{
+								$test = date('d/m/Y', strtotime('+'.$i.' day'));
+								echo("<button class='dropdown-item' onclick='SelectJour();'>$test</button>");
+							}
+						?>
+						</div>
+					</div>
+				</div>
+				
+				<!-- Gestion via PHP à faire -->
+				<div class="col-xl-4">
+					<form>
+						<div class="form-group">
+							<label for="Nom">Nom:</label>
+							<input type="text" class="form-control" id="Nom">
+						</div>
+						<div class="form-group">
+							<label for="Prenom">Prénom:</label>
+							<input type="text" class="form-control" id="Prenom">
+						</div>
+						<div class="form-group">
+							<label for="Numero">Numéro de téléphone:</label>
+							<input type="numero" class="form-control" id="Numero">
+						</div>
+						<div class="form-group">
+							<label for="Email">Email:</label>
+							<input type="email" class="form-control" id="Email">
+						</div>
+						<div>
+							CAPTCHA
+						</div>
+						<div class="d-flex flex-row-reverse">
+							<button type="submit" class="btn btn-primary">Confirmer</button>
+						</div>
+					</form>
+				</div>
+			</div>	
+		</div>
+	</div>
 	
 	<!-- Pied de page -->
 	<footer class="footer">
