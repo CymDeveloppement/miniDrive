3fdf851b520b599913060650a6eaac66c7bc1f57
Benjamin Deschereux <bdeschereux@gmail.com>
2020-06-04 16:49:28 +0200

    nouvelles fonctions pour le tableau de commande

--- js/AcheterBtn.js
+++ js/AcheterBtn.js
@@ -1,4 +1,7 @@
-function AcheterArticle(button, num)
+
+arrayCommande = {};
+arrayPrix = {};
+function AcheterArticle(button, num, nom, prix)
 {
 	var countAchats = document.querySelectorAll(".countAchat"+num);
 	button.style.display = "none";
@@ -6,34 +9,43 @@ function AcheterArticle(button, num)
 	{
 		achatButton.style.display = "block";
 	}
-	document.getElementById("counter"+num).textContent = 1;
+	nom.textContent = 1;
+	arrayCommande[nom.id] = 1;
+	arrayPrix[nom.id] = prix;
 }
-		
-function PlusArticle(num)
-{
-	var countNum = parseInt(document.getElementById("counter"+num).textContent, 10);
-	var countAchats = document.querySelectorAll(".countAchat"+num);
-	
-	document.getElementById("counter"+num).textContent = countNum + 1;
-}
-		
-function MoinsArticle(button, num)
-{
-	var countNum = parseInt(document.getElementById("counter"+num).textContent, 10);
-	var countAchats = document.querySelectorAll(".countAchat"+num);
 			
-	if (document.getElementById("counter"+num).textContent == 1)
+function PlusArticle(num, nom, prix)
+{
+	var countNum = parseInt(nom.textContent, 10);
+	var countAchats = document.querySelectorAll(".countAchat"+num);
+		
+	nom.textContent = countNum + 1;
+	arrayCommande[nom.id] = countNum + 1;
+	arrayPrix[nom.id] = prix;
+}
+			
+function MoinsArticle(button, num, nom, prix)
+{
+	var countNum = parseInt(nom.textContent, 10);
+	var countAchats = document.querySelectorAll(".countAchat"+num);
+				
+	if (nom.textContent == 1)
 	{
-		document.getElementById("counter"+num).textContent = 0;
+		nom.textContent = 0;
 		button.style.display = "block";
 		for (var achatButton of countAchats)
 		{
 			achatButton.style.display = "none";
 		}
+		
+		delete arrayCommande[nom.id];
+		delete arrayPrix[nom.id];
 	}
 	else
 	{
-		document.getElementById("counter"+num).textContent = countNum - 1;
+		nom.textContent = countNum - 1;
+		arrayCommande[nom.id] = countNum - 1;
+		arrayPrix[nom.id] = prix;
 	}
 }
 
@@ -46,4 +58,71 @@ function CacherNb(num)
 		countAchats.style.display = "none";
 	}
 }
+	
+function CountCommande()
+{
+	return(arrayCommande);
+}
+	
+function Commande()
+{
+	var DesacAchats = document.querySelectorAll(".achat");
+	for (var DesacAchat of DesacAchats)
+	{
+		DesacAchat.style.display = "none";
+	}
+	
+	var ActCommandes = document.querySelectorAll(".commande");
+	for (var ActCommande of ActCommandes)
+	{
+		ActCommande.style.display = "block";
+	}
+}
 
+function Retour()
+{
+	var table = $('#Commande').DataTable();
+	var ActCommandes = document.querySelectorAll(".commande");
+	for (var ActCommande of ActCommandes)
+	{
+		ActCommande.style.display = "none";
+	}
+	
+	var DesacAchats = document.querySelectorAll(".achat");
+	for (var DesacAchat of DesacAchats)
+	{
+		DesacAchat.style.display = "block";
+	}
+	table.destroy();
+}
+
+function AfficheTableau()
+{
+	var dataSet = [];
+	var total = 0;
+	for (var objet of Object.keys(arrayCommande))
+	{
+		total += arrayPrix[objet]*arrayCommande[objet];
+		dataSet.push([ objet.replace(/___/g, " "), String(arrayPrix[objet])+"€", arrayCommande[objet], String(arrayPrix[objet]*arrayCommande[objet])+"€" ]);
+	}
+	dataSet.push([ "/", "/", "/", String(total)+"€" ]);
+	
+	$(document).ready(function() {
+		$('#Commande').DataTable( {
+		"pageLength": 5,
+		"order": [[ 3, "desc" ]],
+		data: dataSet,
+			columns: [
+				{ title: "Produits" },
+				{ title: "Prix" },
+				{ title: "Quantité" },
+				{ title: "Total" },
+			]
+		} );
+	} );
+}
+
+function SelectJour()
+{
+	
+}
